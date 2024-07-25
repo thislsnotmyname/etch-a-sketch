7d9f9e9c7646f51636e3ffebf432d0aae7e216a7
JM <143664295+thislsnotmyname@users.noreply.github.com>
2024-07-25 18:51:08 -0500

    Create buttons to clear and create new grid
    Also refined styles and made the grid more responsive to screen size

--- etch.js
+++ etch.js
@@ -1,32 +1,75 @@
-"user strict";
+"use strict";
 // JM, 07/25/2024
 
+const BODY = document.querySelector("body");
 const CONTAINER = document.querySelector("#container");
-let containerWidth = Math.floor(window.innerWidth * 0.5);
+
 let containerHeight = Math.floor(window.innerHeight * 0.7);
 
-let makeGrid = (height, width) => {
-    if (height > 100) height = 100;
-    if (width > 100) width = 100;
+let getUserInput = () => {
+    return Number(prompt("What size should the new grid be? (Max: 100, 1:1 aspect ratio)")) || 16;
+}
 
-    const BOX_WIDTH = Math.floor(containerWidth / width) + "px";
-    const BOX_HEIGHT = Math.floor(containerHeight / height) + "px";
+let randomColor = (box) => {
+    const RANDOM_COLOR = `
+    rgba(
+    ${Math.floor(Math.random() * 256)}, 
+    ${Math.floor(Math.random() * 256)}, 
+    ${Math.floor(Math.random() * 256)}
+    `;
+    let randomColorAndOpacity = `${RANDOM_COLOR}, 1)`;
 
-    for (let i = 0; i < height; i++) {
-        for (let j = 0; j < width; j++) {
+    if (box.getAttribute("data-opacity") < 1) {
+        const NEW_OPACITY = (+box.getAttribute("data-opacity") + 0.1).toFixed(1);
+        box.setAttribute("data-opacity", NEW_OPACITY);
+        randomColorAndOpacity = `${RANDOM_COLOR}, ${NEW_OPACITY})`;
+    } else {
+        box.setAttribute("data-opacity", 1);
+        randomColorAndOpacity = `${RANDOM_COLOR}, 1)`;
+    }
+    return randomColorAndOpacity;
+}
+
+let makeGrid = (size) => {
+    if (size > 100) size = 100;
+    if (document.querySelectorAll(".tiles") !== undefined) {
+        document.querySelectorAll(".tiles").forEach((node) => node.remove());
+    }
+
+    const BOX_SIZE = (containerHeight / size);
+    CONTAINER.style.height = BOX_SIZE * size + 2 + "px";
+
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
             const BOX = document.createElement("div");
             BOX.classList.add("tiles");
+            BOX.setAttribute("data-opacity", 0);
             BOX.style.cssText = 
             `
-                flex: ${BOX_HEIGHT};
-                aspect-ratio: ${height} / ${width};
+                flex: 1 1 ${100 / size}%;
             `;
             CONTAINER.append(BOX);
         }
     }
+
     CONTAINER.addEventListener("mouseover", (e) => {
-        e.target.style.backgroundColor = "black";
+        if (e.target.id === "container") return;
+        e.target.style.backgroundColor = randomColor(e.target);
     });
 }
 
-makeGrid(16, 16);
+let shake = () => {
+    document.querySelectorAll(".tiles").forEach((node) => node.style.backgroundColor = "white");
+}
+
+const NEW_GRID = document.createElement("button");
+NEW_GRID.textContent = "Create New Grid";
+NEW_GRID.addEventListener("click", () => makeGrid(getUserInput()));
+BODY.prepend(NEW_GRID);
+
+makeGrid(16);
+
+const SHAKE = document.createElement("button");
+SHAKE.textContent = "Shake!";
+SHAKE.addEventListener("click", () => shake());
+BODY.append(SHAKE);
